ad0b845252e0bf2a02e110447910a6c6bc0d506b
danddydanddyjohn <julien@venturecopilot.app>
2023-12-29 04:48:46 -0500

    fix: update

--- components/GithubProfileCard.tsx
+++ components/GithubProfileCard.tsx
@@ -20,10 +20,11 @@ const GithubProfileCard = ({ avatar_url, bio, location }: GithubUserType) => {
             <Col lg="8" className="order-lg-1">
               <h2 className="text-white">Reach Out to me!</h2>
               <p className="lead text-white mt-3">DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL</p>
+              <p className="lead text-white mt-3">powerdevsasa@gmail.com</p>
               <p className="text-white mt-3">{bio}</p>
               <div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
                 <i className="ni ni-pin-3 text-info mr-2" />
-                {location}
+                {"Dr Agostina Neta, 38a 9,  Belgrade, 11188"}
               </div>
               <SocialLinks />
             </Col>
